8f485877f64aaf2acb66821f36337650dd89473e
justkali24 <99421494+justkali24@users.noreply.github.com>
2022-03-21 21:43:25 -0400

    Update script.js

--- Module4Solution/script.js
+++ Module4Solution/script.js
@@ -1,7 +1,7 @@
 (function() {
-  const names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
-  for (let i = 0; i < names.length; i++) {
-    let firstLetter = names[i].charAt(0).toLowerCase();
+  var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+  for (var i = 0; i < names.length; i++) {
+    var firstLetter = names[i].charAt(0).toLowerCase();
     if (firstLetter === 'j') {
       byeSpeaker.speak(names[i]);
     } 
@@ -10,3 +10,4 @@
     }
   }
 })();
+

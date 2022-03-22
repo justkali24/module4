a7d8ec0ab4cc698d350c4c285069b05d3f38d99d
justkali24 <99421494+justkali24@users.noreply.github.com>
2022-03-21 21:17:18 -0400

    Update SpeakerGoodBye.js

--- Module4Solution/SpeakerGoodBye.js
+++ Module4Solution/SpeakerGoodBye.js
@@ -1,9 +1,18 @@
-(function(window) {
-  let byeSpeaker = {};
-    byeSpeaker.speak = function() {
-        console.log(speakWord + " " + name);
-    }
-    let speakWord = "Good Bye";
-    window.byeSpeaker = byeSpeaker;
-    }(window));
+// (function(window) {
+//   let byeSpeaker = {};
+//     byeSpeaker.speak = function() {
+//         console.log(speakWord + " " + name);
+//     }
+//     let speakWord = "Good Bye";
+//     window.byeSpeaker = byeSpeaker;
+//     }(window));
 
+const byeSpeaker = {
+	speakWord: 'Good Bye'
+};
+
+(function () {
+	byeSpeaker.speak = function (userName) {
+		return (byeSpeaker.speakWord + " " + userName);
+	}
+}());
